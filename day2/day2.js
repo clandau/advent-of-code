0aedd107e0f6849f8839563fca98f3c77a7610b1
courtney l <clandau@learnics.com>
2020-12-02 18:48:43 -0500

    day 2 - part one working

---
+++ day2/day2.js
@@ -0,0 +1,58 @@
+"use strict";
+exports.__esModule = true;
+var fs = require("fs");
+var path = require("path");
+var inputPath = path.join(__dirname, "./input.txt");
+function processData() {
+    fs.readFile(inputPath, function (err, file) {
+        if (err)
+            return err;
+        var itemArray = file.toString().split("\n");
+        console.log(countValid1(itemArray)); // 454 valid
+        console.log(countValid2(itemArray));
+    });
+}
+function countValid1(passwordData) {
+    // for each item, parse into 3 secctions
+    var invalid = 0;
+    passwordData.forEach(function (elem) {
+        var itemArray = elem.split(" ");
+        var counts = itemArray[0].split("-");
+        var lowBound = parseInt(counts[0]);
+        var highBound = parseInt(counts[1]);
+        var requiredLetter = itemArray[1][0];
+        var password = itemArray[2].split("");
+        var currentCount = 0;
+        for (var _i = 0, password_1 = password; _i < password_1.length; _i++) {
+            var char = password_1[_i];
+            if (char === requiredLetter) {
+                currentCount++;
+                if (currentCount > highBound) {
+                    invalid++;
+                    break;
+                }
+            }
+        }
+        if (currentCount < lowBound)
+            invalid++;
+    });
+    return passwordData.length - invalid;
+}
+function countValid2(passwordData) {
+    var valid = 0;
+    passwordData.forEach(function (elem) {
+        var itemArray = elem.split(" ");
+        var locations = itemArray[0].split("-");
+        var location1 = parseInt(locations[0]) - 1;
+        var location2 = parseInt(locations[1]) - 1;
+        var requiredLetter = itemArray[1][0];
+        var password = itemArray[2];
+        console.log(password, password[location1]);
+        if (password[location1] === requiredLetter || password[location2] === requiredLetter) {
+            console.log(password, location1, location2, locations);
+            valid++;
+        }
+    });
+    return valid;
+}
+processData();
---
+++ day2/day2.ts
@@ -0,0 +1,61 @@
+import { createSecureContext } from "tls";
+
+const fs = require("fs");
+const path = require("path");
+const inputPath = path.join(__dirname, "./input.txt");
+
+function processData() {
+  fs.readFile(inputPath, (err: any, file: any) => {
+    if (err) return err;
+    const itemArray = file.toString().split("\n");
+    console.log(countValid1(itemArray)) // 454 valid
+    console.log(countValid2(itemArray));
+  });
+}
+
+function countValid1(passwordData : string[]) {
+  // for each item, parse into 3 secctions
+ let invalid = 0;
+  passwordData.forEach(elem => {
+    let itemArray = elem.split(" ");
+    const counts = itemArray[0].split("-");
+    const lowBound = parseInt(counts[0]);
+    const highBound = parseInt(counts[1]);
+    const requiredLetter = itemArray[1][0];
+    const password = itemArray[2].split("");
+    let currentCount = 0;
+    for (let char of password) {
+      if (char === requiredLetter) {
+        currentCount++;
+        if (currentCount > highBound) {
+          invalid++;
+          break;
+        }
+      }
+    }
+    if (currentCount < lowBound) invalid++;
+  });
+  return passwordData.length - invalid;
+}
+
+function countValid2(passwordData: string[]) {
+  let valid = 0;
+  passwordData.forEach(elem => {
+    let itemArray = elem.split(" ");
+    const locations: string[] = itemArray[0].split("-");
+    const location1 = parseInt(locations[0]) - 1;
+    const location2 = parseInt(locations[1]) - 1;
+    const requiredLetter = itemArray[1][0];
+    const password = itemArray[2];
+    console.log(password, password[location1]);
+    if (password[location1] === requiredLetter || password[location2] === requiredLetter) {
+      console.log(password, location1, location2, locations)
+      valid++;
+    }
+  })
+  return valid;
+}
+
+processData();
+
+export {};
