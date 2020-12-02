69a10c649f8e9f5ebda0e34d1f051f2abac6fa83
courtney l <clandau@learnics.com>
2020-12-01 19:49:06 -0500

    day 1 final

--- day1/day1.js
+++ day1/day1.js
@@ -6,12 +6,28 @@ function runReportRepair() {
         if (err)
             return err;
         var numberArray = file.toString().split("\n");
-        console.log(numberArray);
-        var number = reportRepair(numberArray);
-        console.log(number);
+        var product1 = reportRepair1(numberArray);
+        var product2 = reportRepair2(numberArray);
+        console.log("Part 1: " + product1);
+        console.log("Part 2: " + product2);
     });
 }
-function reportRepair(numbers) {
+function reportRepair1(numbers) {
+    var goalSum = 2020;
+    var number1;
+    var number2;
+    for (var i = 0; i < numbers.length - 3; i++) {
+        number1 = parseInt(numbers[i]);
+        for (var j = 1; j < numbers.length - 2; j++) {
+            number2 = parseInt(numbers[j]);
+            if (number1 + number2 === goalSum) {
+                return number1 * number2;
+            }
+        }
+    }
+    return -1;
+}
+function reportRepair2(numbers) {
     var goalSum = 2020;
     var number1;
     var number2;
@@ -23,7 +39,6 @@ function reportRepair(numbers) {
             for (var k = 2; k < numbers.length - 1; k++) {
                 number3 = parseInt(numbers[k]);
                 if (number1 + number2 + number3 === goalSum) {
-                    console.log(number1, number2, number3);
                     return number1 * number2 * number3;
                 }
             }
--- day1/day1.ts
+++ day1/day1.ts
@@ -6,33 +6,49 @@ function runReportRepair() {
   fs.readFile(inputPath, (err: any, file: any) => {
     if (err) return err;
     const numberArray = file.toString().split("\n");
-    console.log(numberArray)
-    const number = reportRepair(numberArray);
-    console.log(number)
-
-  })
+    const product1 = reportRepair1(numberArray);
+    const product2 = reportRepair2(numberArray);
+    console.log(`Part 1: ${product1}`);
+    console.log(`Part 2: ${product2}`);
+  });
 }
 
-function reportRepair(numbers: string[]): number {
+function reportRepair1(numbers: string[]): number {
+  const goalSum = 2020;
+  let number1: number;
+  let number2: number;
+  for (let i = 0; i < numbers.length - 3; i++) {
+    number1 = parseInt(numbers[i]);
+    for (let j = 1; j < numbers.length - 2; j++) {
+      number2 = parseInt(numbers[j]);
+      if (number1 + number2 === goalSum) {
+        return number1 * number2;
+      }
+    }
+  }
+  return -1;
+}
+
+function reportRepair2(numbers: string[]): number {
   const goalSum = 2020;
   let number1: number;
   let number2: number;
   let number3: number;
-  for (let i = 0; i < numbers.length-3; i++) {
+  for (let i = 0; i < numbers.length - 3; i++) {
     number1 = parseInt(numbers[i]);
-    for (let j = 1; j < numbers.length-2; j++) {
+    for (let j = 1; j < numbers.length - 2; j++) {
       number2 = parseInt(numbers[j]);
-      for (let k = 2; k< numbers.length-1; k++) {
+      for (let k = 2; k < numbers.length - 1; k++) {
         number3 = parseInt(numbers[k]);
         if (number1 + number2 + number3 === goalSum) {
-          console.log(number1, number2, number3)
           return number1 * number2 * number3;
         }
       }
-
     }
   }
   return -1;
 }
 
 runReportRepair();
+// Part 1: 1007331
+// Part 2: 48914340
