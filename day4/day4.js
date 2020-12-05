8c09ed3ef0b5249a6c4c14eeefeb9837069fdee3
courtney l <clandau@learnics.com>
2020-12-05 12:02:57 -0500

    part 2 complete

--- day4/day4.js
+++ day4/day4.js
@@ -4,9 +4,7 @@ var fs = require("fs");
 var data = fs
     .readFileSync("./day4/input.txt", "utf-8")
     .split("\n\n");
-var expectedFields = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid", "cid"];
 var actualExpectedFields = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"];
-console.log(data);
 function countValidPassports(input) {
     var totalInvalidPassports = 0;
     for (var _i = 0, input_1 = input; _i < input_1.length; _i++) {
@@ -16,21 +14,74 @@ function countValidPassports(input) {
         for (var _a = 0, fieldsArray_1 = fieldsArray; _a < fieldsArray_1.length; _a++) {
             var i = fieldsArray_1[_a];
             var start = i.split(":")[0];
-            // console.log(start);
-            // console.log(actualExpectedFields);
             if (actualExpectedFields.indexOf(start) !== -1) {
-                // console.log(actualExpectedFields.indexOf(start))
                 countFields++;
             }
         }
-        console.log("number fields: " + countFields);
         if (countFields < actualExpectedFields.length) {
             totalInvalidPassports++;
-            console.log(JSON.stringify(passport));
         }
     }
-    // console.log(countFields)
-    // console.log(i)
     return input.length - totalInvalidPassports;
 }
-console.log(countValidPassports(data));
+function countValidPassports2(input) {
+    var totalInvalidPassports = 0;
+    for (var _i = 0, input_2 = input; _i < input_2.length; _i++) {
+        var passport = input_2[_i];
+        var fieldsArray = passport.split(/\s/g);
+        var countFields = 0;
+        for (var _a = 0, fieldsArray_2 = fieldsArray; _a < fieldsArray_2.length; _a++) {
+            var i = fieldsArray_2[_a];
+            var inputs = i.split(":");
+            var start = inputs[0];
+            var value = inputs[1];
+            if (actualExpectedFields.indexOf(start) !== -1 && isValidData(start, value)) {
+                countFields++;
+            }
+        }
+        if (countFields < actualExpectedFields.length) {
+            totalInvalidPassports++;
+        }
+    }
+    return input.length - totalInvalidPassports;
+}
+function isValidData(item, value) {
+    var passportItems = {
+        byr: validNumberRange(parseInt(value), 1920, 2002),
+        iyr: validNumberRange(parseInt(value), 2010, 2020),
+        eyr: validNumberRange(parseInt(value), 2020, 2030),
+        hgt: validHeight(value),
+        hcl: value.length === 7 && validHairColor(value),
+        ecl: ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"].includes(value),
+        pid: value.length === 9 && isNumber(value)
+    };
+    return passportItems[item];
+}
+function validNumberRange(val, min, max) {
+    return val >= min && val <= max;
+}
+function validHeight(hgt) {
+    var units = hgt.slice(-2);
+    var value = hgt.slice(0, -2);
+    if (["cm", "in"].includes(units)) {
+        if (units === "cm") {
+            // cm 150 and at most 193
+            return validNumberRange(parseInt(value), 150, 193);
+        }
+        else if (units === "in") {
+            // in at least 59 and at most 76
+            return validNumberRange(parseInt(value), 59, 76);
+        }
+    }
+    return false;
+}
+function validHairColor(color) {
+    var regex = /^#([0-9a-f])*$/g;
+    return regex.test(color);
+}
+function isNumber(val) {
+    var allNumberRegex = /^([0-9])*$/g;
+    return allNumberRegex.test(val);
+}
+console.log(countValidPassports(data)); // 233
+console.log(countValidPassports2(data)); // 111
--- day4/day4.ts
+++ day4/day4.ts
@@ -1,12 +1,20 @@
 const fs = require("fs");
+type PassportItems = {
+  byr: boolean;
+  iyr: boolean;
+  eyr: boolean;
+  hgt: boolean;
+  hcl: boolean;
+  ecl: boolean;
+  pid: boolean;
+};
+
+type PassportItemValues = keyof PassportItems;
 
 const data: string[] = fs
   .readFileSync("./day4/input.txt", "utf-8")
   .split("\n\n");
-const expectedFields = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid", "cid"];
 const actualExpectedFields = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"];
-
-console.log(data)
 
 function countValidPassports(input: string[]) {
   let totalInvalidPassports = 0;
@@ -25,6 +33,71 @@ function countValidPassports(input: string[]) {
   }
   return input.length - totalInvalidPassports;
 }
+
+function countValidPassports2(input: string[]) {
+  let totalInvalidPassports = 0;
+  for (let passport of input) {
+    const fieldsArray = passport.split(/\s/g);
+    let countFields = 0;
+    for (let i of fieldsArray) {
+      const inputs = i.split(":")
+      const start = <PassportItemValues>inputs[0];
+      const value = inputs[1];
+      if (actualExpectedFields.indexOf(start) !== -1 && isValidData(start, value)) {
+        countFields++;
+      }
+    }
+    if (countFields < actualExpectedFields.length) {
+      totalInvalidPassports++;
+    }
+  }
+  return input.length - totalInvalidPassports;
+}
+
+function isValidData(item: PassportItemValues, value: string): boolean {
+  const passportItems: PassportItems = {
+    byr: validNumberRange(parseInt(value), 1920, 2002),
+    iyr: validNumberRange(parseInt(value), 2010, 2020),
+    eyr: validNumberRange(parseInt(value), 2020, 2030),
+    hgt: validHeight(value),
+    hcl: value.length === 7 && validHairColor(value),
+    ecl: ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"].includes(value),
+    pid: value.length === 9 && isNumber(value),
+  };
+
+  return passportItems[item];
+}
+
+function validNumberRange(val: number, min: number, max: number) {
+  return val >= min && val <= max;
+}
+
+function validHeight(hgt: string) {
+  const units = hgt.slice(-2);
+  const value = hgt.slice(0, -2);
+  if (["cm", "in"].includes(units)) {
+    if (units === "cm") {
+      // cm 150 and at most 193
+      return validNumberRange(parseInt(value), 150, 193);
+    } else if (units === "in") {
+      // in at least 59 and at most 76
+      return validNumberRange(parseInt(value), 59, 76);
+    }
+  }
+  return false;
+}
+
+function validHairColor(color: string) {
+  const regex = /^#([0-9a-f])*$/g;
+  return regex.test(color);
+}
+
+function isNumber(val: string) {
+  const allNumberRegex = /^([0-9])*$/g;
+  return allNumberRegex.test(val);
+}
+
 console.log(countValidPassports(data)); // 233
+console.log(countValidPassports2(data)); // 111
 
 export {};
