ad9b0aebee2e6fcb8658d13deb62e07024e0160e
courtney l <clandau@learnics.com>
2020-12-06 10:18:33 -0500

    day 6 - part 2 complete & refactored

--- day6/6.js
+++ day6/6.js
@@ -2,9 +2,27 @@ const fs = require("fs");
 const DATA = fs.readFileSync("./day6/input.txt", "utf-8").split("\n\n");
 function customCustoms1(answerGroups) {
     let sumOfCounts = 0;
-    answerGroups.forEach(answer => {
-        sumOfCounts += new Set(answer.split("\n").join("").split("")).size;
+    answerGroups.forEach(group => {
+        sumOfCounts += new Set(group.split("\n").join("").split("")).size;
     });
     return sumOfCounts;
 }
-console.log(customCustoms1(DATA));
+function customCustoms2(answerGroups) {
+    let sumOfCounts = 0;
+    answerGroups.forEach(group => {
+        const personGroup = group.split("\n");
+        const numberOfPeople = personGroup.length;
+        const answerHolder = {};
+        personGroup.forEach(person => {
+            for (let i of person) {
+                answerHolder[i] ? answerHolder[i]++ : answerHolder[i] = 1;
+            }
+        });
+        Object.entries(answerHolder).forEach(count => {
+            count[1] === numberOfPeople ? sumOfCounts++ : null;
+        });
+    });
+    return sumOfCounts;
+}
+console.log(`Part 1: ${customCustoms1(DATA)}`);
+console.log(`Part 2: ${customCustoms2(DATA)}`); // Part 2: 3354
--- day6/6.ts
+++ day6/6.ts
@@ -3,12 +3,31 @@ const DATA: string[] = fs.readFileSync("./day6/input.txt", "utf-8").split("\n\n"
 
 function customCustoms1(answerGroups: string[]) {
   let sumOfCounts = 0;
-  answerGroups.forEach(answer => {
-    sumOfCounts += new Set(answer.split("\n").join("").split("")).size
+  answerGroups.forEach(group => {
+    sumOfCounts += new Set(group.split("\n").join("").split("")).size
   })
   return sumOfCounts;
 }
 
-console.log(customCustoms1(DATA))
+function customCustoms2(answerGroups: string[]) {
+  let sumOfCounts = 0;
+  answerGroups.forEach(group => {
+    const personGroup = group.split("\n");
+    const numberOfPeople = personGroup.length;
+    const answerHolder: any = {};
+    personGroup.forEach(person => {
+      for (let i of person) {
+        answerHolder[i] ? answerHolder[i]++ : answerHolder[i] = 1;
+      }
+    })
+    Object.entries(answerHolder).forEach(count => {
+      count[1] === numberOfPeople ? sumOfCounts++ : null;
+    })
+  })
+  return sumOfCounts;
+}
+
+console.log(`Part 1: ${customCustoms1(DATA)}`) // Part 1: 6799
+console.log(`Part 2: ${customCustoms2(DATA)}`) // Part 2: 3354
 
 export {}
