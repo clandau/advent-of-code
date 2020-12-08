aab4526ed9f2f8fd812016b0ed7655529c957ddf
courtney l <clandau@learnics.com>
2020-12-07 20:39:59 -0500

    part 2 done - still needs refactoring

--- day7/7.js
+++ day7/7.js
@@ -2,6 +2,7 @@
 exports.__esModule = true;
 var fs = require("fs");
 var DATA = fs.readFileSync("./day7/input.txt", "utf-8").split("\n");
+var testData = fs.readFileSync("./day7/practiceInput.txt", "utf-8").split("\n");
 function HandyHaversacks1(rules) {
     var bagColors = {};
     for (var _i = 0, rules_1 = rules; _i < rules_1.length; _i++) {
@@ -42,4 +43,38 @@ function HandyHaversacks1(rules) {
     countOccurances(["shiny gold"]);
     return totalCount - 1;
 }
-console.log("Part 1: " + HandyHaversacks1(DATA));
+function HandyHaversacks2(rules) {
+    var bagCollection = {};
+    for (var _i = 0, rules_2 = rules; _i < rules_2.length; _i++) {
+        var rule = rules_2[_i];
+        var splitOnContain = rule.split(" contain ");
+        var holdingBag = splitOnContain[0].split(" ").slice(0, -1).join(" ");
+        if (!bagCollection[holdingBag]) {
+            bagCollection[holdingBag] = {};
+        }
+        var containingBags = splitOnContain[1].split(", ");
+        for (var _a = 0, containingBags_2 = containingBags; _a < containingBags_2.length; _a++) {
+            var bag = containingBags_2[_a];
+            if (bag !== "no other bags.") {
+                var bagDetails = bag.split(" ");
+                var count = bagDetails.shift();
+                var bagDescription = bag.split(" ").slice(1, -1).join(" ");
+                count ? bagCollection[holdingBag][bagDescription] = parseInt(count) : null;
+            }
+        }
+    }
+    var totalCount = 0;
+    function countTotalBags(item, numberOfItems) {
+        if (numberOfItems === void 0) { numberOfItems = 1; }
+        totalCount += numberOfItems;
+        if (!bagCollection[item])
+            return;
+        Object.keys(bagCollection[item]).forEach(function (key) {
+            return countTotalBags(key, bagCollection[item][key] * numberOfItems);
+        });
+    }
+    console.log(countTotalBags("shiny gold"));
+    return totalCount - 1;
+}
+// console.log(`Part 1: ${HandyHaversacks1(DATA)}`); // Part 1: 257
+console.log("Part 2: " + HandyHaversacks2(DATA));
--- day7/7.ts
+++ day7/7.ts
@@ -1,6 +1,8 @@
+import { createSecureContext } from "tls";
+
 const fs = require("fs");
 const DATA: string[] = fs.readFileSync("./day7/input.txt", "utf-8").split("\n");
-
+const testData: string[] = fs.readFileSync("./day7/practiceInput.txt", "utf-8").split("\n");
 
 function HandyHaversacks1(rules: string[]) {
   const bagColors: any = {}
@@ -37,8 +39,41 @@ function HandyHaversacks1(rules: string[]) {
   countOccurances(["shiny gold"])
   return totalCount - 1;
 }
+
+function HandyHaversacks2(rules: string[]) {
+  const bagCollection: any = {}
+  for (let rule of rules) {
+    const splitOnContain = rule.split(" contain ");
+    const holdingBag = splitOnContain[0].split(" ").slice(0, -1).join(" ");
+    if (!bagCollection[holdingBag]) {
+      bagCollection[holdingBag] = {};
+    }
+    const containingBags = splitOnContain[1].split(", ");
+    for (let bag of containingBags) {
+      if (bag !== "no other bags.") {
+        let bagDetails = bag.split(" ");
+        const count = bagDetails.shift();
+        let bagDescription = bag.split(" ").slice(1, -1).join(" ");
+        count ? bagCollection[holdingBag][bagDescription] = parseInt(count) : null;
+      }
+    }
+  }
+  let totalCount = 0;
+  
+  function countTotalBags(item: string, numberOfItems = 1) : void {
+    totalCount += numberOfItems;
+    if (!bagCollection[item]) return;
+    Object.keys(bagCollection[item]).forEach(key => {
+      return countTotalBags(key, bagCollection[item][key] * numberOfItems)
+    })
+  }
+
+  countTotalBags("shiny gold")
+  return totalCount - 1;
+}
  
-console.log(`Part 1: ${HandyHaversacks1(DATA)}`); // Part 1: 257
+// console.log(`Part 1: ${HandyHaversacks1(DATA)}`); // Part 1: 257
+console.log(`Part 2: ${HandyHaversacks2(DATA)}`);  // Part 2: 1038
 
 
 export {};
