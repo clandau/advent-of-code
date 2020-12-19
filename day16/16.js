d61dc10bb52ccc462daed72871f0617e412974d9
courtney l <clandau@learnics.com>
2020-12-19 11:59:19 -0500

    day 16 - part 2 done

--- day16/16.js
+++ day16/16.js
@@ -1,56 +1,56 @@
 "use strict";
 exports.__esModule = true;
 var fs = require("fs");
-var data = fs.readFileSync("./day16/testInput.txt", "utf-8").split("\n\n");
-var invalidTickets = [];
-// const invalidTickets = [
-//   12,
-//   22,
-//   977,
-//   10,
-//   8,
-//   985,
-//   977,
-//   5,
-//   985,
-//   15,
-//   991,
-//   8,
-//   999,
-//   999,
-//   990,
-//   21,
-//   979,
-//   989,
-//   976,
-//   14,
-//   984,
-//   991,
-//   976,
-//   998,
-//   989,
-//   13,
-//   3,
-//   984,
-//   23,
-//   986,
-//   16,
-//   11,
-//   990,
-//   10,
-//   4,
-//   10,
-//   6,
-//   4,
-//   980,
-//   7,
-//   6,
-//   14,
-//   5,
-//   22,
-//   989,
-//   995,
-// ];
+var data = fs.readFileSync("./day16/input.txt", "utf-8").split("\n\n");
+// const invalidTickets: number[] = [];
+var invalidTickets = [
+    12,
+    22,
+    977,
+    10,
+    8,
+    985,
+    977,
+    5,
+    985,
+    15,
+    991,
+    8,
+    999,
+    999,
+    990,
+    21,
+    979,
+    989,
+    976,
+    14,
+    984,
+    991,
+    976,
+    998,
+    989,
+    13,
+    3,
+    984,
+    23,
+    986,
+    16,
+    11,
+    990,
+    10,
+    4,
+    10,
+    6,
+    4,
+    980,
+    7,
+    6,
+    14,
+    5,
+    22,
+    989,
+    995,
+];
 function ticketTranslation1() {
     var invalidTickets = [];
     var ranges = parseRanges(data[0]);
@@ -75,57 +75,79 @@ function ticketTranslation1() {
 }
 function ticketTranslation2() {
     var ranges = parseRangesKeepRows(data[0]);
-    var ticketLocationCounts = {};
-    var fieldArray = Object.entries(ranges);
+    var fieldKeys = Object.keys(ranges);
     var fieldValues = Object.values(ranges);
     var ticketRows = parseNearbyTicketsKeepRows(data[2]);
+    console.log(ticketRows);
+    var ticketLength = ticketRows[0].length;
     var locationCountHolder = new Array(fieldValues.length);
     ticketRows.forEach(function (ticket) {
-        var _loop_2 = function (i) {
+        var _loop_1 = function (i) {
             var ticketInt = parseInt(ticket[i]);
-            var _loop_3 = function (j) {
-                fieldValues[j].forEach(function (f) {
+            var _loop_2 = function (j_1) {
+                fieldValues[j_1].forEach(function (f) {
                     var min = parseInt(f[0]);
                     var max = parseInt(f[1]);
                     if (ticketInt >= min && ticketInt <= max) {
                         if (locationCountHolder[i]) {
-                            locationCountHolder[i][j] ? locationCountHolder[i][j] += 1 : locationCountHolder[i][j] = 1;
+                            locationCountHolder[i][j_1]
+                                ? (locationCountHolder[i][j_1] += 1)
+                                : (locationCountHolder[i][j_1] = 1);
                         }
                         else {
                             locationCountHolder[i] = [];
-                            locationCountHolder[i][j] = 1;
+                            locationCountHolder[i][j_1] = 1;
                         }
-                        console.log(locationCountHolder);
                     }
                 });
             };
-            for (var j = 0; j < fieldValues.length; j++) {
-                _loop_3(j);
+            for (var j_1 = 0; j_1 < fieldValues.length; j_1++) {
+                _loop_2(j_1);
             }
         };
         for (var i = 0; i < ticket.length; i++) {
-            _loop_2(i);
+            _loop_1(i);
         }
     });
-    console.log(locationCountHolder);
-    var locations = [];
-    var _loop_1 = function (i) {
-        // if only one is in all of them, that will be the correct index
-        var locationsWithAll = [];
-        locationCountHolder[i].forEach(function (item) {
-            if (item === ticketRows.length) {
-                locationsWithAll.push(i);
+    var columnHolder = {};
+    var foundFields = [];
+    while (Object.keys(columnHolder).length < ticketLength) {
+        for (var i = 0; i < locationCountHolder.length; i++) {
+            var found = [];
+            for (var j_2 = 0; j_2 < locationCountHolder[i].length; j_2++) {
+                if (locationCountHolder[i][j_2] === ticketRows.length) {
+                    var field = fieldKeys[j_2];
+                    if (!Object.values(columnHolder).includes(field)) {
+                        // if this FIELD is in there, move don't add it
+                        found.push(j_2);
+                    }
+                }
             }
-        });
-        if (locationsWithAll.length === 1) {
-            locations[i] = locationsWithAll[0];
+            if (found.length === 1) {
+                var location_1 = found[0];
+                columnHolder[i] = fieldKeys[location_1];
+                foundFields.push(fieldKeys[location_1]);
+                locationCountHolder[i] = [];
+            }
         }
-        console.log(locationsWithAll);
-    };
-    for (var i = 0; i < locationCountHolder.length; i++) {
-        _loop_1(i);
     }
-    console.log(locations);
+    var myTicket = data[1].split("\n")[1].split(",");
+    var departureValues = [];
+    console.log(columnHolder);
+    for (var i = 0; i < myTicket.length; i++) {
+        console.log(columnHolder[i] + ": " + myTicket[i]);
+        if (columnHolder[i].includes("departure")) {
+            departureValues.push(parseInt(myTicket[i]));
+        }
+        console.log("departureValues: " + departureValues);
+    }
+    var product = departureValues[0] * departureValues[1];
+    var j = 2;
+    while (j < departureValues.length) {
+        product *= departureValues[j];
+        j++;
+    }
+    return product;
 }
 function parseRanges(data) {
     var ranges = [];
@@ -161,17 +183,19 @@ function parseNearbyTicketsKeepRows(data) {
     var rows = data.split("\n");
     rows.shift();
     var filteredRows = [];
+    var currentSplitRow;
     rows.forEach(function (row) {
+        currentSplitRow = row.split(",");
         var found = false;
         for (var _i = 0, invalidTickets_1 = invalidTickets; _i < invalidTickets_1.length; _i++) {
             var ticket = invalidTickets_1[_i];
-            if (row.indexOf(ticket.toString()) !== -1) {
+            if (currentSplitRow.indexOf(ticket.toString()) !== -1) {
                 found = true;
                 break;
             }
         }
         if (!found)
-            filteredRows.push(row.split(","));
+            filteredRows.push(currentSplitRow);
     });
     return filteredRows;
 }
--- day16/16.ts
+++ day16/16.ts
@@ -1,56 +1,54 @@
-import { sep } from "path";
-
 const fs = require("fs");
-const data = fs.readFileSync("./day16/testInput.txt", "utf-8").split("\n\n");
-const invalidTickets: number[] = [];
-// const invalidTickets = [
-//   12,
-//   22,
-//   977,
-//   10,
-//   8,
-//   985,
-//   977,
-//   5,
-//   985,
-//   15,
-//   991,
-//   8,
-//   999,
-//   999,
-//   990,
-//   21,
-//   979,
-//   989,
-//   976,
-//   14,
-//   984,
-//   991,
-//   976,
-//   998,
-//   989,
-//   13,
-//   3,
-//   984,
-//   23,
-//   986,
-//   16,
-//   11,
-//   990,
-//   10,
-//   4,
-//   10,
-//   6,
-//   4,
-//   980,
-//   7,
-//   6,
-//   14,
-//   5,
-//   22,
-//   989,
-//   995,
-// ];
+const data = fs.readFileSync("./day16/input.txt", "utf-8").split("\n\n");
+// const invalidTickets: number[] = [];
+const invalidTickets = [
+  12,
+  22,
+  977,
+  10,
+  8,
+  985,
+  977,
+  5,
+  985,
+  15,
+  991,
+  8,
+  999,
+  999,
+  990,
+  21,
+  979,
+  989,
+  976,
+  14,
+  984,
+  991,
+  976,
+  998,
+  989,
+  13,
+  3,
+  984,
+  23,
+  986,
+  16,
+  11,
+  990,
+  10,
+  4,
+  10,
+  6,
+  4,
+  980,
+  7,
+  6,
+  14,
+  5,
+  22,
+  989,
+  995,
+];
 
 function ticketTranslation1() {
   let invalidTickets: number[] = [];
@@ -74,53 +72,74 @@ function ticketTranslation1() {
 }
 function ticketTranslation2() {
   const ranges: string[][] = parseRangesKeepRows(data[0]);
-  const ticketLocationCounts: any = {}
-  const fieldArray = Object.entries(ranges);
+  const fieldKeys = Object.keys(ranges);
   const fieldValues = Object.values(ranges);
   const ticketRows = parseNearbyTicketsKeepRows(data[2]);
+  const ticketLength = ticketRows[0].length;
   const locationCountHolder: number[][] = new Array(fieldValues.length);
   ticketRows.forEach((ticket) => {
     for (let i = 0; i < ticket.length; i++) {
-      let ticketInt = parseInt(ticket[i])
+      let ticketInt = parseInt(ticket[i]);
       for (let j = 0; j < fieldValues.length; j++) {
-        fieldValues[j].forEach(f => {
+        fieldValues[j].forEach((f) => {
           let min = parseInt(f[0]);
           let max = parseInt(f[1]);
           if (ticketInt >= min && ticketInt <= max) {
             if (locationCountHolder[i]) {
-              locationCountHolder[i][j] ? locationCountHolder[i][j] += 1 : locationCountHolder[i][j] = 1;
+              locationCountHolder[i][j]
+                ? (locationCountHolder[i][j] += 1)
+                : (locationCountHolder[i][j] = 1);
             } else {
               locationCountHolder[i] = [];
               locationCountHolder[i][j] = 1;
             }
-            console.log(locationCountHolder);
           }
-        })
+        });
       }
     }
-  })
-  console.log(locationCountHolder)
-  const locations = [];
-  for (let i = 0; i< locationCountHolder.length; i++) {
-    // if only one is in all of them, that will be the correct index
-    let locationsWithAll: number[] = [];
-    locationCountHolder[i].forEach(item => {
-      if (item === ticketRows.length) {
-        locationsWithAll.push(i);
+  });
+  let columnHolder: any = {};
+  let foundFields: string[] = [];
+
+  while (Object.keys(columnHolder).length < ticketLength) {
+    for (let i = 0; i < locationCountHolder.length; i++) {
+      let found = [];
+      for (let j = 0; j < locationCountHolder[i].length; j++) {
+        if (locationCountHolder[i][j] === ticketRows.length) {
+          const field = fieldKeys[j];
+          if (!Object.values(columnHolder).includes(field)) {
+            // if this FIELD is in there, move don't add it
+            found.push(j);
+          }
+        }
       }
-    })
-    if (locationsWithAll.length === 1) {
-      locations[i] = locationsWithAll[0]
+      if (found.length === 1) {
+        let location = found[0];
+        columnHolder[i] = fieldKeys[location];
+        foundFields.push(fieldKeys[location]);
+        locationCountHolder[i] = [];
+      }
     }
-    console.log(locationsWithAll);
   }
-  console.log(locations)
+  const myTicket = data[1].split("\n")[1].split(",");
+  let departureValues: number[] = [];
+  for (let i = 0; i < myTicket.length; i++) {
+    if (columnHolder[i].includes("departure")) {
+      departureValues.push(parseInt(myTicket[i]));
+    }
+  }
+  let product = departureValues[0] * departureValues[1];
+  let j = 2;
+  while (j < departureValues.length) {
+    product *= departureValues[j];
+    j++;
+  }
+  return product;
 }
 
 function parseRanges(data: string) {
   const ranges: string[][] = [];
   let fieldRows = data.split("\n").join(" ").split(" ");
-  console.log(fieldRows);
   fieldRows.forEach((row) => {
     if (row.includes("-")) {
       let rangeInts = row.split("-");
@@ -154,20 +173,22 @@ function parseNearbyTicketsKeepRows(data: string) {
   const rows = data.split("\n");
   rows.shift();
   const filteredRows: string[][] = [];
+  let currentSplitRow;
   rows.forEach((row) => {
+    currentSplitRow = row.split(",");
     let found = false;
     for (let ticket of invalidTickets) {
-      if (row.indexOf(ticket.toString()) !== -1) {
+      if (currentSplitRow.indexOf(ticket.toString()) !== -1) {
         found = true;
         break;
       }
     }
-    if (!found) filteredRows.push(row.split(","));
+    if (!found) filteredRows.push(currentSplitRow);
   });
   return filteredRows;
 }
 
 // console.log(`part 1: ${ticketTranslation1()}`); // part 1: 21978
-console.log(`part 2: ${ticketTranslation2()}`); //
+console.log(`part 2: ${ticketTranslation2()}`); // part 2: 1053686852011
 
 export {};
